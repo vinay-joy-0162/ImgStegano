64921a28d0648f61dbeb15925698ccbdff3a835a
vinay-joy-0162 <67511735+vinay-joy-0162@users.noreply.github.com>
2023-08-03 03:55:40 +0100

    new-pro-progress

---
+++ src/components/NavComp.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default class NavComp extends Component {
+
+    render () {
+        return (
+            <Navbar expand="lg" className="bg-dark-subtle">
+      <Container>
+        <Navbar.Brand href="#home">ImgSteg</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Encode</Nav.Link>
+            <Nav.Link href="#link">Decode
+            {/* <Nav.Link href="#link">Sign-in
+            </Nav.Link> */}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        )
+    }
+}
